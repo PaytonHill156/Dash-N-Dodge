82420af2fff6efef4dacb4b4d8c97b2a087f9f71
PaytonHill156 <hillpr@student.swosu.edu>
2020-01-11 12:55:35 -0600

    first commit

---
+++ Assets/Scripts/RandomSounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSounds : MonoBehaviour
+{
+    private AudioSource source;
+    public AudioClip[] sounds;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        int randomSounds = Random.Range(0, sounds.Length);
+        source.clip = sounds[randomSounds];
+        source.Play();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Spawner : MonoBehaviour
+{
+    public Transform[] spawnpoints;
+    public GameObject[] enemies;
+
+    private float timeBtwSpawns;
+    public float startTimeBtwSpawns;
+    public float minTimeBtwSpawns;
+    public float decrease;
+
+    public GameObject player;
+    public Text scoreDisplay;
+    float score = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            if (timeBtwSpawns <= 0)
+            {
+                //spawn enemy
+                Transform randomSpawnPoint = spawnpoints[Random.Range(0, spawnpoints.Length)];
+                GameObject randomEnemy = enemies[Random.Range(0, enemies.Length)];
+
+                if (startTimeBtwSpawns > minTimeBtwSpawns)
+                {
+                    startTimeBtwSpawns -= decrease;
+                }
+                //spawn enemy at random spawn points
+                Instantiate(randomEnemy, randomSpawnPoint.position, Quaternion.identity);
+                score++;
+                timeBtwSpawns = startTimeBtwSpawns;
+            }
+            else
+            {
+                timeBtwSpawns -= Time.deltaTime;
+
+            }
+            scoreDisplay.text = "Score: " + score.ToString();
+
+        }
+    }
+}
